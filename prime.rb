2ae46a58651cd972763d0f82fa926946ea428507
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 03:59:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(integer)
+ if integer <= 1
+   return false
+   (2..integer -1).each do |number|
+     if (integer % number) == 0
+       return false
+   end# Add  code here!
+  end
+  true
+ end
