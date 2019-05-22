a2945aaacd98c667d6db4a26a2e1eb5a4702be16
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 04:01:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 def prime?(integer)
- if integer <= 1
+ if integer >= 2
    (2..integer -1).each do |number|
      if (integer % number) == 0
        return false
    end# Add  code here!
-   true
   end
+  true
+end
